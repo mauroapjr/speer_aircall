5d088994d17f51f9db90ea9fe8fc7ce5c21f2a5b
mauroapjr <mauroapjr@outlook.com>
2023-08-06 10:51:07 -0400

    update package.json and .babelrc files with updated versions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import { getCalls } from './util/api';
 
 import Header from './Header.jsx';
 
+
+  
+
 const App = () => {
+  const [calls, setCalls] = useState([]);
+
+  useEffect(() => {
+    // Fetch the list of calls from the API when the component mounts
+    getCalls()
+      .then((response) => setCalls(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className='container'>
       <Header/>
       <div className="container-view">Some activities should be here</div>
+
+      <h2>Activity Feed</h2>
+      <ul>
+        {calls.map((call) => (
+          <li key={call.id}>{call.direction}</li>
+        ))}
+      </ul>
+      
     </div>
   );
 };
--- src/util/api.js
+++ src/util/api.js
@@ -1,2 +1,23 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/';
+
+// get calls from the Activity Feed
+export const getCalls = () => {
+  return axios.get(`${BASE_URL}activities`);
+};
+
+// get a specific call detail
+export const getCallDetail = (callId) => {
+  return axios.get(`${BASE_URL}activities/${callId}`);
+};
+
+// update a call 
+export const updateCall = (callId, data) => {
+  return axios.patch(`${BASE_URL}activities/${callId}`, data);
+};
+
+// reset all calls 
+export const resetCalls = () => {
+  return axios.patch(`${BASE_URL}reset`);
+};
